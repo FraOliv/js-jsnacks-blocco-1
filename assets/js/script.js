98d7d2636a2739b561e369bbf0eb7a9d99ceed7c
FRANCESCO <OLIVOTTI>
2020-12-09 08:50:54 +0100

    almost complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,23 +23,23 @@ if (Scelta == "si"){
 
 /* Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
 Stampare a schermo la bici con peso minore. */
-/* 
-var Bikes = [{
+
+/* var Bikes = [{
         "nome": "bici1",
-        "peso": "5",
+        "peso": 5,
     },
 
     {
         "nome": "bici2",
-        "peso": "25",
+        "peso": 25,
     },
     {
         "nome": "bici3",
-        "peso": "40",
+        "peso": 40,
     },
     {
         "nome": "bici4",
-        "peso": "10",
+        "peso": 10,
     },
 ]
 
@@ -54,8 +54,8 @@ for (var index = 0; index < Bikes.length; index++) {
 }
 
 console.log(pesoMinimo);
-console.log(Bikes[index]);
- */
+console.log(Bikes[index]); */
+
 
 /*     Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
 Calcolare perimetro e area.
@@ -254,6 +254,8 @@ Dividi in due array separati le zucchine che misurano
 meno o più di 15cm.
 Infine stampa separatamente quanto pesano i due gruppi
 di zucchine */
+
+/* 
      var Zucchine  = [
     {
     "varieta": "faenza",
@@ -340,3 +342,64 @@ zucchineLunghe.forEach((element) => {
     console.log(element.peso)
 }); 
 
+ */
+
+/* 
+ Scrivi una funzione che fonda due array (con lo stesso
+numero di elementi) prendendo alternativamente gli
+elementi da uno e dall’altro
+es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]. */
+
+/* 
+
+function splicer(array, element, index) {
+    array.splice(index * 2, 0, element);
+    return array;
+}
+
+function merge(array1, array2) {
+    return array1.reduce(splicer, array2.slice());
+}
+
+var array1= [1,2,3,4,5,6];
+var array2 = ["A", "B", "C", "D", "E", "F"]
+
+var MergedArray = merge(array1, array2);
+
+console.log(MergedArray); */
+
+
+/* Scrivi una funzione che accetti tre argomenti:
+un array e due numeri (“a” più piccolo di “b” e “b” grande al
+massimo quanto il numero di elementi dell’array).
+La funzione ritornerà un nuovo array con i valori che
+hanno la posizione compresa tra “a” e “b” */
+
+
+/* Scrivi una funzione che accetti tre argomenti: */
+function ElementsBetweenNumAandNumB(array1,numA,numB){
+/* 
+un array e due numeri */
+    var numA;
+    var numB;
+    var array;
+
+
+  var ElementsinRange = [];
+/* 
+grande al massimo quanto il numero di elementi dell’array */
+  for (var i = 0; i <= array.length; i++) {
+/*     (“a” più piccolo di “b” e “b” grande al
+massimo quanto il numero di elementi dell’array)   */
+    if (i <= numB && i >= numA )  {
+      ElementsinRange.push(array[i]);
+    }
+  }
+  return ElementsinRange;
+}
+
+
+var arrayData = ["Lorena", "Lello", "Giorgiunes", "Fabiunes", "Serena", "Camelia"];
+var startData = 2;
+var endData = 4;
+console.log(ElementsBetweenNumAandNumB(arrayData,startData,endData));
